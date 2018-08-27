0bcd62d810b612286caaee5871b535749e4faf9b
Juan Sebastián Millán Lejarde <js.millan10@uniandes.edu.co>
2018-08-26 21:22:08 -0500

    Casi todo listo

--- src/js/main.js
+++ src/js/main.js
@@ -12,7 +12,7 @@ var jumbo = document.getElementById("jumbo_titulo");
 var navNombre = document.getElementById("nombre_nav");
 var medida = jumbo.offsetHeight;
 function myFunction() {
-  if (window.pageYOffset>=medida)
+  if ((window.pageYOffset+30)>medida)
   {
     navNombre.classList.remove("invisible");
   }
@@ -44,32 +44,6 @@ $(function() {
       tb.css({left: ((viewport - divWidth-300)*((e.pageX / viewport).toFixed(3))).toFixed(1) + 300 + "px" });
     });
   });
-
-  $('.timeline ul li').on('click', function(){
-    $(this).parent().blink();
-  });
 });
 
-"use strict";
-$.getJSON("json/skills.json", function (data) {
-  $("#skillsList").append(data.map(function (obj) {
-    var titulo = $("<h3>")
-      .addClass("SkillTitle")
-      .text(obj.name);
-    var barra = $("<div>")
-      .addClass("progress-bar")
-      .attr("id", obj.name)
-      .attr("role", "progressbar")
-      .attr("aria-valuemin", "0")
-      .attr("aria-valuemax", "100")
-      .attr("aria-valuenow", obj.skillLevel)
-      .text(obj.skillLevel + "%")
-      .css("width", obj.skillLevel + "%");
-    var progress = $("<div>")
-      .addClass("progress")
-      .append(barra);
-    return $("<div>")
-      .append(titulo)
-      .append(progress);
-  }));
-});
+
